c045d82f5b49c1fe63e4a3cda5c9aa9b3b5a3032
Alexander Tuna <alexander.tuna@gmail.com>
2024-07-26 18:40:44 -0500

    Add ap23_ to files to specify their usage at ap23 host

--- grid/ap23_condor_exe.sh
+++ grid/ap23_condor_exe.sh
@@ -13,7 +13,7 @@ echo
 
 echo "Copying data from /ospool/uc-shared/project/"
 export STASHCP=/cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/23/current/el8-x86_64/usr/bin/stashcp
-$STASHCP -d osdf:///ospool/uc-shared/project/futurecolliders/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/photonGun_E_250_1000/photonGun_E_250_1000_reco_0.slcio ./photonGun_E_250_1000_reco_0.slcio
+$STASHCP -d osdf:///ospool/uc-shared/project/muoncollider/tutorial2024/photonGun_E_250_1000/photonGun_E_250_1000_reco_0.slcio ./photonGun_E_250_1000_reco_0.slcio
 ls -ltrh
 anajob -m 10 photonGun_E_250_1000_reco_0.slcio
 rm -f photonGun_E_250_1000_reco_0.slcio
